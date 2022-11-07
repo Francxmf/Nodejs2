8bc167cab68bc10b32687f7dd0287c1686e65707
Francesco M Ferri <franko@MinidiFrancesco.homenet.telecomitalia.it>
2022-11-07 10:00:45 +0100

    Completion of EX3'do stuff with resources'

--- app.ts
+++ app.ts
@@ -13,10 +13,18 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/planets", async (request, response) => {
-  const planets = await prisma.planet.findMany();
+app.get("/planets/:id(\\d+)", async (request, response) => {
+  const planetId = Number(request.params.id);
+  const planet = await prisma.planet.findUnique({
+    where: { id: planetId },
+  });
 
-  response.json(planets);
+  if (!planet) {
+    response.status(404);
+    return next(`Cannot GET /planets/${planetId}`);
+  }
+
+  response.json(planet);
 });
 
 app.post(
@@ -32,6 +40,42 @@ app.post(
     response.status(201).json(planet);
   }
 );
+
+app.put(
+  "/planets/:id(\\d+)",
+  validate({ body: planetSchema }),
+  async (request, response, next) => {
+    const planetId = Number(request.params.id);
+    const planetData: PlanetData = request.body;
+
+    try {
+      const planet = await prisma.planet.update({
+        where: { id: planetId },
+        data: planetData,
+      });
+
+      response.status(200).json(planet);
+    } catch (error) {
+      response.status(404);
+      next(`Cannot PUT /planets/${planetId}`);
+    }
+  }
+);
+
+app.delete("/planets/:id(\\d+)", async (request, response, next) => {
+  const planetId = Number(request.params.id);
+
+  try {
+    await prisma.planet.delete({
+      where: { id: planetId },
+    });
+
+    response.status(200).end();
+  } catch (error) {
+    response.status(404);
+    next(`Cannot DELETE /planets/${planetId}`);
+  }
+});
 
 app.use(validationErrorMiddleware);
 
